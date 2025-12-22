7f8ec36ba19c4ebef300f16b82a5441439fed948
Sanket Darunkar <sanketdarunkar111@gmail.com>
2025-12-22 16:14:07 +0530

    Initial project structure with secure configuration

---
+++ src/main/java/com/educationloan/els/EducationLoanSystemApplication.java
@@ -0,0 +1,13 @@
+package com.educationloan.els;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationLoanSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationLoanSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/educationloan/els/EducationLoanSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educationloan.els;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationLoanSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
