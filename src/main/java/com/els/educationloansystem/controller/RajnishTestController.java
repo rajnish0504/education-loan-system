19500c77507144261e94aff3485295cac5be5e59
Rajnish Singh <103492286+rajnish0504@users.noreply.github.com>
2025-12-27 01:23:17 +0530

    testing

--- src/main/java/com/els/educationloansystem/controller/RajnishTestController.java
+++ src/main/java/com/els/educationloansystem/controller/RajnishTestController.java
@@ -1,12 +1,18 @@
 package com.els.educationloansystem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/rajnish")
 public class RajnishTestController {
     
-    
+    @PostMapping("/tester")
+    public String testing(){
+            return "Testing Done";
+    }
+
 
 }
