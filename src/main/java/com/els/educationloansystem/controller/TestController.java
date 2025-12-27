04896f7ab7dbb41cad2146ba0c649e8fa1a2df31
Pratiksha Patil <patilpratiksha2611@gmail.com>
2025-12-27 17:13:16 +0530

    Update TestController.java

--- src/main/java/com/els/educationloansystem/controller/TestController.java
+++ src/main/java/com/els/educationloansystem/controller/TestController.java
@@ -2,6 +2,8 @@ package com.els.educationloansystem.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class TestController {
@@ -10,7 +12,11 @@ public class TestController {
     public String test() {
         return "Controller working";
     }
-    @Getmapp
+    @GetMapping("path")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
     public void check()
     {
     	
