0b20d57612169653d88a8cb06c266decc7176259
Rajnish Singh <103492286+rajnish0504@users.noreply.github.com>
2026-01-14 18:26:00 +0530

    Update AdminController.java

--- src/main/java/com/els/educationloansystem/controller/AdminController.java
+++ src/main/java/com/els/educationloansystem/controller/AdminController.java
@@ -1,25 +1,74 @@
 package com.els.educationloansystem.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import com.els.educationloansystem.jwt.JWTRequest;
+import com.els.educationloansystem.jwt.JWTResponse;
+import com.els.educationloansystem.jwt.JwtUtil;
 import com.els.educationloansystem.service.LoanApplicationService;
 
 @RestController
 @RequestMapping("/api/admin")
+@CrossOrigin("*")
 public class AdminController {
 
     @Autowired
     private LoanApplicationService loanApplicationService;
+    
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+    
+    @PostMapping("/login")
+    public JWTResponse adminLogin(@RequestBody JWTRequest request) {
+
+        Authentication authentication = authenticationManager
+                .authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                    )
+                );
+
+        if (authentication.isAuthenticated()) {
+            String token = jwtUtil.generateToken(
+                    request.getEmail(),
+                    "ADMIN"
+            );
+            return new JWTResponse(token);
+        }
+
+        throw new RuntimeException("Invalid admin credentials");
+    }
+    
+    
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/applications")
+    public ResponseEntity<?> getAllApplications() {
+        return ResponseEntity.ok(
+            this.loanApplicationService.getAllApplicationsForAdmin()
+        );
+    }
+
+    
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/approve/{applicationId}")
     public ResponseEntity<String> approveLoan(@PathVariable Long applicationId) {
 
         loanApplicationService.approveLoan(applicationId);
         return ResponseEntity.ok("Loan Approved Successfully");
     }
+    
+    
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/reject/{applicationId}")
     public ResponseEntity<String> rejectLoan(
             @PathVariable Long applicationId,
